7764eb4f6540158a22b0d0f25d61e1eec5a522fc
Jeffrey Prouty <jcprouty@gmail.com>
2022-01-29 14:30:17 -0500

    Allows user to mark activity engaged

--- pages/what-to-do.js
+++ pages/what-to-do.js
@@ -1,21 +1,18 @@
 import React from "react";
-import useSWR from "swr";
-import { useRouter } from "next/router";
-import Immutable from "immutable";
 import PageWrapper from "../lib/components/page-wrapper";
 import NewItemBar from "../lib/components/new-item-bar";
 import ActivityTable from "../lib/components/activity-table";
-import { fetcher } from "../lib/utils";
+import { useActivities } from "../lib/utils";
 
 function WhatToDo() {
-  const router = useRouter();
-  const { data, error } = useSWR(["/api/activities", router.query.mode], fetcher);
-  // TODO use hook in both children instead of parent
+  const { activities, errorGettingActivities } = useActivities();
   return (
     <PageWrapper>
       <h1>Things I might do</h1>
-      <NewItemBar activities={data} />
-      {!data ? <span>Loading...</span> : <ActivityTable activities={data} />}
+      {errorGettingActivities && <span>Error Loading Data</span>}
+      {!activities && <span>Loading...</span>}
+      {activities && <NewItemBar />}
+      {activities && <ActivityTable />}
     </PageWrapper>
   );
 }
